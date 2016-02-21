83abb732b9ea72c8465e5158b0b7592fed373699
John Hewitt <john.hewitt64@gmail.com>
2016-02-21 00:36:08 -0500

    completed dependency parse-parser, output sentences in all levels of granularity

--- parse_parser.py
+++ parse_parser.py
@@ -21,13 +21,56 @@ argp.add_argument('difficulty', help='integer, from 1 to 3, which determines\
         the level of granularity of the parse')
 
 args = argp.parse_args()
+depth = int(args.difficulty)
 
+
+def extract_text(tup, depth):
+    '''
+    Extracts text from a stanford sentence dependency parse tuple.
+    Initial input is a sentence-tuple
+    All text assumed to be in 2-tuples of form (POS, token). 
+    Inserts ||| delimiters until depth of granularity reached.
+    '''
+    temp = ''
+    if depth > 0:
+        temp = '||| '
+
+    # Iterate over all elements in current tuple
+    for elem in tup:
+        if isinstance(elem, tuple):
+            # Look one level deeper to see if next tuple encodes text
+            if len(elem) == 2\
+                    and isinstance(elem[0], str)\
+                    and isinstance(elem[1], str):
+                        if depth > 1:
+                            temp = temp + " ||| " + elem[1]
+                        else:
+                            temp = temp + " " + elem[1] #Return the word, not the POS
+            #Else, walk further down the tuple
+            else:
+                temp = temp + extract_text(elem, depth - 1)
+        # Strings outside of a 2-tuple are POS tags. Ignore,
+        # but match to this case to avoid TypeError
+        elif isinstance(elem, str):
+            pass
+        # Signifies malformed sentence parse tuple
+        else:
+            raise TypeError
+    return temp
+
+
+
+# stdin should be a file containing sentence parses.
+# sentences may take arbitrary numbers of lines.
+# Subsequent sentences must be delimited by an empty line
 while stdin:
     line = ''
     try:
         line = stdin.next()
     except StopIteration:
         pass
+
+    # Parse a single sentence
     sentence_buffer = ''
     while line.strip(' ') != '\n':
         sentence_buffer +=  filter(lambda x: x in string.printable, line.strip()) + ' '
@@ -40,21 +83,19 @@ while stdin:
 
     if not line:
         break
-    print len(sentence_buffer)
+
+    # Format sentence text, eval into tuple
     sentence_buffer = sentence_buffer.strip()[1:-1]
     sentence_buffer = re.sub(r'[A-Z\+\/\-\.\?\d\*\`\,\'\$\!\%\:\;\\]+', lambda x: '"' + x.group() + '"' , sentence_buffer, flags=re.IGNORECASE)
-    print len(sentence_buffer)
     sentence_array = sentence_buffer.split()
     sentence_tuple_string = ', '.join(sentence_array)
-    #sentence_tuple_string = re.sub(r'[A-Z\-\.\?\d\*\`]+', lambda x: '"' + x.group() + '"' , sentence_tuple_string, flags=re.IGNORECASE)
-    #print sentence_tuple_string
-    print eval(sentence_tuple_string)
-   # print(eval(sentence_tuple_string)), type(eval(sentence_tuple_string))
+    sentence_tuple = eval(sentence_tuple_string)
 
-def quote(m):
-    return  '"' + m.groups(0) + '"'
+    # extract text, splitting to desired granularity
+    sentence = re.sub(r'( *\|\|\| *){1,4}', ' ||| ', extract_text(sentence_tuple, depth))
 
+    # Remove prepended '||| '
+    sentence = sentence[4:]
 
-
-
-
+    # Print to stdout
+    print sentence
